c2ef89445895b1299fbe5e86c44135bd1c13ab4d
dzholoeva_e <dzholoeva_e@auca.kg>
2021-10-08 15:04:49 +0600

    uri1004 solved

---
+++ uri problems/src/uri1004.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class uri1004 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int a = inp.nextInt();
+        int b = inp.nextInt();
+        int mul = a*b;
+        System.out.println("PROD = " + mul );
+    }
+}
