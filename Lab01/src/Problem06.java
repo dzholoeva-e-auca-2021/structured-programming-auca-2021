500fcf5b62898bf4fbb2c9600c55da62c401d20a
dzholoeva_e <dzholoeva_e@auca.kg>
2021-10-08 14:52:03 +0600

    Problem06 solved

--- Lab01/src/Problem06.java
+++ Lab01/src/Problem06.java
@@ -1,2 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Problem06 {
+    public static void main(String[] args) {
+        System.out.println("    J      A      V      V      A   ");
+        System.out.println("    J     A  A     V    V      A  A");
+        System.out.println("J   J    AAAAAA     V  V      AAAAAA");
+        System.out.println(" J J    A      A      V      A      A");
+    }
 }
