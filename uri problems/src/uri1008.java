1e2d5950701b6b3202e73e5d42057373aeabe636
Админ <dzholoeva_e@auca.kg>
2021-11-13 18:44:43 +0600

    uri1008 accepted

---
+++ uri problems/src/uri1008.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class uri1008 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int a = inp.nextInt();
+        int b = inp.nextInt();
+        double c = inp.nextDouble();
+        double salary = b*c ;
+        System.out.println("NUMBER = " + a);
+        System.out.printf("SALARY = U$ %.2f", salary);
+    }
+}
