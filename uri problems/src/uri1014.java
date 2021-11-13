d36a48ad2fec4f75c0118319de4e67300d2587c9
Админ <dzholoeva_e@auca.kg>
2021-11-13 19:02:52 +0600

    uri1014 presentation error

---
+++ uri problems/src/uri1014.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+    public class uri1014 {
+        public static void main(String[] args) {
+            Scanner inp = new Scanner(System.in);
+            int X = inp.nextInt();
+            double Y = inp.nextDouble();
+            double fuel = X/Y ;
+            System.out.printf("%.3f", fuel);
+            System.out.print(" km/l");
+        }
+    }
+
+
