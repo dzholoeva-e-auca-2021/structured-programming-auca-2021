b5e6886d0c90d43fc36b0aa77dce86682cf3d95f
Админ <dzholoeva_e@auca.kg>
2021-11-13 18:39:33 +0600

    uri1006 accepted

---
+++ uri problems/src/uri1006.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class uri1006 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double A = inp.nextDouble();
+        double B = inp.nextDouble();
+        double C = inp.nextDouble();
+        double average = (2*A + 3*B + 5*C)/(2+3+5);
+        System.out.printf("MEDIA = %.1f", average);
+    }
+}
