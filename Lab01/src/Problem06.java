9cd97bb29b942f7b69c54a2c95b727f9674bcee9
Админ <dzholoeva_e@auca.kg>
2021-11-19 10:20:46 +0600

    problem08 solved

--- Lab01/src/Problem06.java
+++ Lab01/src/Problem06.java
@@ -1,7 +1,10 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Problem06 {
     public static void main(String[] args) {
+        Scanner inp = new Scanner (System.in);
+        String java = inp.next();
         System.out.println("    J      A      V      V      A   ");
         System.out.println("    J     A  A     V    V      A  A");
         System.out.println("J   J    AAAAAA     V  V      AAAAAA");
---
+++ Lab01/src/Problem08.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int a  = 1+2+3+4+5+6+7+8+9 ;
+        System.out.println("1+2+3+4+5+6+7+8+9="+ a);
+
+    }
+}
