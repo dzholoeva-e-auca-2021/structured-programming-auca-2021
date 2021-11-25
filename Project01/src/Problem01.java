e1eed55f1bd6e060167b9dcb8e7e48a2f017ee7e
Админ <dzholoeva_e@auca.kg>
2021-11-26 02:19:15 +0600

    project01 Problem01

---
+++ Project01/src/Problem01.java
@@ -0,0 +1,20 @@
+import processing.core.*;
+
+public class Problem01 extends PApplet {
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+    }
+
+    public void draw() {
+        background(100, 100, 100);
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Problem01");
+    }
+
+}}
