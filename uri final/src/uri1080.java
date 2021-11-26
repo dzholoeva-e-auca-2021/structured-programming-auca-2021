a5de51783c12da81e87c2185cebcfd87c330e092
Админ <dzholoeva_e@auca.kg>
2021-11-26 15:29:18 +0600

    uri1080 redacted

--- uri final/src/uri1080.java
+++ uri final/src/uri1080.java
@@ -1,14 +1,11 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class uri1080 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
 
-        Random rnd = new Random();
         int i = 0;
         while (i < 101) {
-            System.out.println(0 + rnd.nextInt(100000));
             ++i;
         }
     }
