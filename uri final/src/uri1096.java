29e54bcbd1cb9031f0d09175591e3c4f41737d04
Админ <dzholoeva_e@auca.kg>
2021-11-30 15:53:34 +0600

    uri1096 accepted

---
+++ uri final/src/uri1096.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class uri1096 {
+        public static void main(String[] args) {
+            Scanner inp = new Scanner(System.in);
+
+
+            for (int I = 1; I<10; I+=2) {
+                ;
+            for (int J = 7; J >= 5; J--) {
+
+                System.out.println("I=" + I + " J=" + J);
+            }
+        }
+
+    }
+
+}
