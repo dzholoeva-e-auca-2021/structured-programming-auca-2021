64bedf3c3571f502c0c295e833ee32bee07f1be8
Админ <dzholoeva_e@auca.kg>
2021-11-30 15:16:29 +0600

    uri1080

--- uri final/src/uri1080.java
+++ uri final/src/uri1080.java
@@ -4,9 +4,6 @@ public class uri1080 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
 
-        int i = 0;
-        while (i < 101) {
-            ++i;
         }
     }
 }
