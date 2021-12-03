b4803db4e5d18ffdd887c18bcf18b367ca55ceb3
Админ <dzholoeva_e@auca.kg>
2021-12-03 15:26:42 +0600

    Project01-01

--- Project01/src/Problem01.java
+++ Project01/src/Problem01.java
@@ -4,17 +4,33 @@ public class Problem01 extends PApplet {
 
     public void settings() {
         fullScreen();
+        size(200, 200);
+    }
+
+    @Override
+    public void selectOutput(String prompt, String callback) {
     }
 
     public void setup() {
+        background(0);
+
+
+
     }
 
     public void draw() {
-        background(100, 100, 100);
+        textSize(100);
+        fill(255, 0, 0);
+        text("Hello, Processing!!!", 100, 100);
+
+
     }
 
     public static void main(String[] args) {
         PApplet.main("Problem01");
     }
 
+
+
 }
+
