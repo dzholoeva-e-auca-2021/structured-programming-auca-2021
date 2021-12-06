0a03b0b9bf46da4d2b30b5669c4045a578286783
Админ <dzholoeva_e@auca.kg>
2021-12-07 01:50:25 +0600

    Project01-01

--- Project01/src/Problem01.java
+++ Project01/src/Problem01.java
@@ -1,36 +1,37 @@
 import processing.core.*;
 
 public class Problem01 extends PApplet {
-
+    float messageSize = 10 ;
     public void settings() {
         fullScreen();
-        size(200, 200);
-    }
 
-    @Override
-    public void selectOutput(String prompt, String callback) {
+
     }
 
     public void setup() {
         background(0);
-
+        textAlign(CENTER, CENTER);
 
 
     }
 
     public void draw() {
-        textSize(100);
+        background(0, 0, 0);
+        textSize(messageSize);
         fill(255, 0, 0);
-        text("Hello, Processing!!!", 200, 400);
-
+        text("Hello, Processing!!!", width/2.0f , height/2.0f);
+        if (messageSize < 130) {
+            messageSize += 1;
+        }
 
     }
 
-    public static void main(String[] args) {
+
+            public static void main
+                (String[]args) {
         PApplet.main("Problem01");
-    }
+            }
 
+        }
 
-
-}
 
