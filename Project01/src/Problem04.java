660042e2a4d8d2ba130c4e355719b929f5dfe862
Админ <dzholoeva_e@auca.kg>
2021-12-12 20:32:16 +0600

    Project01-04

--- Project01/src/Problem04.java
+++ Project01/src/Problem04.java
@@ -4,6 +4,8 @@ public class Problem04 extends PApplet {
     float circleX;
     float circleY;
     float circleR;
+    float pointX;
+    float pointY;
     float dx;
     float dy;
     public void settings() {
@@ -14,39 +16,48 @@ public class Problem04 extends PApplet {
         circleX = width /2f ;
         circleY = height/2f ;
         circleR = height/20f;
+        pointX = random(0, width);
+        pointY = random(0, height);
         frameRate(10);
 
     }
 
     public void draw() {
-        fill(0,0,0,50);
+        fill(0, 0, 0, 50);
         rect(0, 0, width, height);
 
-        fill(255,0,0);
+        fill(255, 255, 0);
+        circle(pointX, pointY, circleR);
+
+        fill(255, 0, 0);
         circle(circleX, circleY, circleR);
 
-        if (key == CODED){
+        if (key == CODED) {
             switch (keyCode) {
                 case UP:
-                circleY-= circleR;
-                break;
+                    circleY -= circleR;
+                    break;
                 case DOWN:
-                circleY += circleR;
-                break;
+                    circleY += circleR;
+                    break;
                 case LEFT:
-                    circleX-=circleR;
+                    circleX -= circleR;
                     break;
                 case RIGHT:
-                    circleX+=circleR;
+                    circleX += circleR;
                     break;
-
             }
-        }
-        fill(0,0,255);
-        for (int i=0; i<width; i++)
-        for (int i=0; i<height; i++);
-    }
 
+                stroke(0, 0, 255);
+            for (int i = 0; i < width; i++)
+                line(i * circleR, 0, i * circleR, height);
+
+            stroke(0, 0, 255);
+            for (int i = 0; i < height; i++)
+                line(0, i * circleR, width, i * circleR);
+
+        }
+    }
     public static void main(String[] args) {
         PApplet.main("Problem04");
     }
