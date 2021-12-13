56102ca42b9a4278b367d694d13dbe46dfc8f167
Админ <dzholoeva_e@auca.kg>
2021-12-14 01:07:14 +0600

    project-01-06

--- Project01/src/Problem06.java
+++ Project01/src/Problem06.java
@@ -3,6 +3,11 @@ import processing.core.*;
 public class Problem06 extends PApplet {
 float alpha;
 float beta;
+final float Num_of_circles = 50;
+    float r = height / 2f;
+    float comp = 5;
+    float dr = r / Num_of_circles;
+    float dComp = 255 / Num_of_circles;
     public void settings() {
         fullScreen();
     }
@@ -15,28 +20,29 @@ float beta;
         background(0, 0, 0);
 
         pushMatrix();
-        translate(width/2f, height/2f);
+        translate(width / 2f, height / 2f);
         rotate(alpha);
-        stroke(255,0,0);
-        strokeWeight(5);
-        line(-50, 0, 50, 0);
-        line(0,50,0,-50);
-        alpha +=0.05f;
-        beta +=0.1f;
+        alpha += 0.05f;
+        beta += 0.1f;
 
-        translate(150, 0);
-        circle(0, 0, 50);
+       fill(50,0,0);
+        circle(CENTER, CENTER, r);
 
-        rotate(beta);
-        translate(80, 0);
-        circle(0, 0, 10);
 
-        popMatrix();
-        textSize(36);
-        textAlign(CENTER, CENTER);
-        text("Planets",width/2f, 100  );
+            translate(150, 0);
+            circle(0, 0, 50);
 
-    }
+            rotate(beta);
+            translate(80, 0);
+            circle(0, 0, 10);
+
+            popMatrix();
+            textSize(36);
+            textAlign(CENTER, CENTER);
+            text("Planets", width / 2f, 100);
+
+
+        }
 
     public static void main(String[] args) {
         PApplet.main("Problem06");
