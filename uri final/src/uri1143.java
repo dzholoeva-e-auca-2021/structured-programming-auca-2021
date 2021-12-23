90364125f065935b750de957ea08401740f60979
Админ <dzholoeva_e@auca.kg>
2021-12-24 02:31:14 +0600

    uri1143 is accepted

---
+++ uri final/src/uri1143.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class uri1143 {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        int N = inp.nextInt();
+        if (N>1 && N<1000) {
+            for (int i = 1; i <= N; i++) {
+                System.out.printf("%s %s %s%n", i, i*i, i*i*i);
+            }
+        }
+    }
+}
