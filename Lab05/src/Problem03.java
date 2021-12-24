1ce117982be09c2d0acd3fe6c86fd0df908409ce
Админ <dzholoeva_e@auca.kg>
2021-12-24 11:10:07 +0600

    lab05

--- Lab05/src/Problem03.java
+++ Lab05/src/Problem03.java
@@ -4,17 +4,15 @@ import java.util.Scanner;
 public class Problem03 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        //System.out.println((int)(100 * Math.random());
         System.out.println("Number of tests?");
         int nTest = inp.nextInt();
 
 
         Random rnd = new Random();
         int i = 0;
+        int numOfCorrectAnswers = 0;
         while (i < nTest) {
-            int numOfCorrectAnswers = 0;
             int x = -50 + rnd.nextInt(101);
-            ;
             int y = -50 + rnd.nextInt(101);
             System.out.printf("%d + %d = ", x, y);
             int userAnswer = inp.nextInt();
@@ -23,5 +21,7 @@ public class Problem03 {
             }
             ++i;
         }
+        System.out.println("Number of correct unswers"+ numOfCorrectAnswers);
+        System.out.println("Number of incorrect unswers"+ (nTest - numOfCorrectAnswers));
     }
 }
