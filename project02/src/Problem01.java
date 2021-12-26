7c28934a9daa553537d8a27457672936950c75f4
Админ <dzholoeva_e@auca.kg>
2021-12-26 20:47:44 +0600

    project02

--- project02/src/Problem01.java
+++ project02/src/Problem01.java
@@ -1,7 +1,10 @@
 import processing.core.*;
 
 public class Problem01 extends PApplet {
-
+float x1;
+float y1;
+    float x2;
+    float y2;
     public void settings() {
         fullScreen();
 
@@ -25,8 +28,18 @@ public class Problem01 extends PApplet {
 
     }
     public void draw() {
+        fill(0,0,255);
+        square(width/3.5f, height/4.5f, 500);
+
+       line(x1, y1, x2, y2);
+       stroke(140, 140, 140);
+        for (float x1= width / 3.5f; x1 < 500; x1 += (500 / 4f)) {
+            for (float y1 = height / 3.5f; y1 < 500; y1 += (500 / 4f)) {
+            }
+        }
 
     }
+
     public static void main(String[] args) {
         PApplet.main("Problem01");
     }
